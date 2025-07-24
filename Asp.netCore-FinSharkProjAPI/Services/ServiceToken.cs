9a03e4716c3797cd09a0681ab0bd093b56bc2648
Muhammad Uzair Arif <uzairarif28@gmail.com>
2025-07-25 01:50:23 +0500

    Token comments added

--- Asp.netCore-FinSharkProjAPI/Services/ServiceToken.cs
+++ Asp.netCore-FinSharkProjAPI/Services/ServiceToken.cs
@@ -14,16 +14,21 @@ namespace Asp.netCore_FinSharkProjAPI.Services
         public ServiceToken(IConfiguration config)
         {
             this.config = config;
-            // SymmetricSecurityKey
+            // JWT:SigningKey config file (appsettings.json) se ek secret key le raha hai.
             key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(config["Jwt:SigningKey"]));
         }
         public string CreateToken(AppUser user)
         {
+            /* JWT ke andar kuch data pack karte hain. Isse kehte hain Claims.
+               Tum JWT ke andar Email aur UserName bhej rahe ho. */
             var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim(JwtRegisteredClaimNames.GivenName, user.UserName)
             };
+
+            /* JWT ko sign karne ke liye key aur HmacSha256Signature algorithm use kiya gaya.
+               Signing ka matlab: koi token fake na bana sake. */
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
 
             var tokenDescriptor = new SecurityTokenDescriptor
@@ -36,9 +41,10 @@ namespace Asp.netCore_FinSharkProjAPI.Services
 
             };
 
+            // JWT token create karne ke liye JwtSecurityTokenHandler ka use kiya gaya. TokenHandler JWT generate karta hai.
             var tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
+            var token = tokenHandler.CreateToken(tokenDescriptor); //CreateToken() — JWT object banata hai.
+            return tokenHandler.WriteToken(token);  //WriteToken() — us object ko string mein convert karta hai (jo frontend ko bhejoge).
 
         }
     
