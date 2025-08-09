db033013733eb002a8f8718d0d52d6591c1f61c9
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-10 00:20:21 +0500

    State n Search component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './App.css';
 
 import CardList from './components/Cardlist/CardList';
+import Search from './components/Search/Search';
 
 const App = () => {
   return (
     <div className="app">
+      <Search/>
       <CardList />
     </div>
   )
