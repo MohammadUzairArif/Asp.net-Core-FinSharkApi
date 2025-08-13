1bd39c1a924b60510827a3d4bdcba9a6cf23a0af
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-13 21:37:58 +0500

    Event

--- Frontend/src/components/Search/Search.tsx
+++ Frontend/src/components/Search/Search.tsx
@@ -1,17 +1,21 @@
-import React, { useState, type JSX } from "react";
 
+import React, { type ChangeEvent, useState, type SyntheticEvent, type JSX } from "react";
 type Props = {};
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
   const [search, setSearch] = useState<string>("");
 
-  const onClick = (e: any) => {
+   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    console.log(e);
+  };
+  const onClick = (e: SyntheticEvent) => {
     console.log(e);
   };
   return (
     <div>
-      <input value={search} onChange={(e) => onClick(e)}></input>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
     </div>
   );
 };
