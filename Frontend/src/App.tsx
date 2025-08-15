236a30ad6304046ba675aaff734a0bd260e4480d
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-15 21:40:44 +0500

    ternary operator

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,22 +3,37 @@ import './App.css';
 
 import CardList from './components/Cardlist/CardList';
 import Search from './components/Search/Search';
+import type { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 const App = () => {
     const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+  const [serverError, setServerError] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
-    console.log(e);
+   
   };
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+   const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    //setServerError(result.data);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+
+    }
+    
   };
+
   return (
     <div className="app">
       <Search onClick={onClick} search={search} handleChange={handleChange}/>
       <CardList />
+        {serverError && <div>Unable to connect to API</div>}
+      {/* {serverError ? <div>Connected</div> : <div>Unable to connect to api</div>} */}
     </div>
   )
 }
--- Frontend/src/api.tsx
+++ Frontend/src/api.tsx
@@ -9,7 +9,7 @@ import type  {
   CompanySearch,
   CompanyTenK,
   CompanyHistoricalDividend,
-  Dividend,
+  
 } from "./company";
 
 
