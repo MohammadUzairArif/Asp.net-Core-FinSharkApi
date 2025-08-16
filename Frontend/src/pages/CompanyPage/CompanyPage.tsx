5ed838a88aa1ad404ec08da8b1572fc2cda31087
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-16 18:52:50 +0500

    Company profile daashboard

--- Frontend/src/pages/CompanyPage/CompanyPage.tsx
+++ Frontend/src/pages/CompanyPage/CompanyPage.tsx
@@ -2,12 +2,19 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getCompanyProfile } from "../../api";
 import type { CompanyProfile } from "../../company";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import CompanyDashboard from "../../components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../components/Tile/Tile";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
   let { ticker } = useParams();
+
+  
+
   const [company, setCompany] = useState<CompanyProfile>();
+const [activeSidebarItem, setActiveSideBarItem] = useState<number>(1);
 
   useEffect(() => {
     const getProfileInit = async () => {
@@ -20,7 +27,12 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div className="company-profile-container">{company.companyName}</div>
+       <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard>
+             <Tile title="Company Name" subTitle={company.companyName} />
+          </CompanyDashboard>
+        </div>
       ) : (
         <div>Company Not Found!</div>
       )}
