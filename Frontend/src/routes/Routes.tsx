80919ccb37ae5466695199f32d5d6ac5f7e9b01e
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-16 21:01:33 +0500

    Table

--- Frontend/src/routes/Routes.tsx
+++ Frontend/src/routes/Routes.tsx
@@ -5,6 +5,7 @@ import SearchPage from "../pages/SearchPage/SearchPage";
 import CompanyPage from "../pages/CompanyPage/CompanyPage";
 import CompanyProfile from "../components/CompanyProfile/CompanyProfile";
 import IncomeStatement from "../components/IncomeStatement/IncomeStatement";
+import DesignGuide from "../pages/DesignGuide/DesignGuide";
 
 export const router = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
+      { path: "design-guide", element: <DesignGuide /> },
       {
         path: "company/:ticker",
         element: <CompanyPage />,
