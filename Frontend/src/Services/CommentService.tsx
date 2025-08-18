b366b3e8a08655194858867ddf3bc4c641aad3fe
user.name like : "uzairarif28@gmail.com <uzairarif28@gmail.com>
2025-08-18 19:36:57 +0500

    Read comments

--- Frontend/src/Services/CommentService.tsx
+++ Frontend/src/Services/CommentService.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { CommentPost } from "../Models/Comment";
+import type { CommentGet, CommentPost } from "../Models/Comment";
 import { handleError } from "../Helpers/ErrorHandler";
 
 
@@ -15,6 +15,16 @@ export const commentPostAPI = async (
       title: title,
       content: content,
     });
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+
+export const commentGetAPI = async (symbol: string) => {
+  try {
+    const data = await axios.get<CommentGet[]>(api + `?Symbol=${symbol}`);
     return data;
   } catch (error) {
     handleError(error);
